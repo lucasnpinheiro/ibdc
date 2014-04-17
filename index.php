ab7a557f0e1f23d9d7d517f1c1178e242e015581
lucas <lucas@localhost.localdomain>
2014-04-17 17:44:28 -0300

    IBDC

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Instituto IBDC</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" /> 
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css" /> 
+
+    </head>
+    <body>
+        <div class="header">
+            <div class="nav">
+                <div class="row conteudo_nav">
+                    <div class="col-md-3 col-md-12">
+                        logo
+                    </div>
+                    <div class="col-md-6 col-md-12">
+                        menu
+                    </div>
+                    <div class="col-md-3 col-md-12">
+                        social
+                    </div>
+                </div>
+            </div>
+            <div class="conteudo_banner">
+                <span class="titulo">
+                    SAIBA TUDO SOBRE
+                </span>
+                <span class="texto">
+                    O INSTITUTO IBDC
+                </span>
+            </div>
+            <div class="conteudo_galeria">
+                <div class="fundo"></div>
+                <ul>
+                    <li class="mini_foto caixa_1">1</li>
+                    <li class="mini_foto caixa_2">2</li>
+                    <li class="mini_foto caixa_3">3</li>
+                    <li class="mini_foto caixa_4">4</li>
+                    <li class="mini_foto caixa_5">5</li>
+                    <li class="mini_foto caixa_6">6</li>
+                    <li class="mini_foto caixa_7">7</li>
+                    <li class="mini_foto caixa_8">8
+                        <span class="seta"> <i class="fa fa-chevron-right"></i> </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
